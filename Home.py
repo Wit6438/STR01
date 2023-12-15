f60146138babb43355309934bec721a8bcb5cc7c
Wit6438 <vbvbvb585@hotmail.com>
2023-12-15 15:00:18 +0700

    .commit

--- Home.py
+++ Home.py
@@ -74,13 +74,13 @@ if st.button("ทำนายผล"):
    out=Knn_model.predict(x_input)
 
    if out[0]=="Setosa":
-      #st.image("./pic/iris.jpg")
+      st.image("./pic/3.jpg")
       st.header("Setosa")
    elif out[0]=="Versicolor":
-      #st.image("./pic/iris2.jpg")
+      st.image("./pic/4.jpg")
       st.header("Versicolor")
    else:
-      #st.image("./pic/iris1.jpg")  
+      st.image("./pic/5.jpg")  
       st.header("Verginiga")
    st.button("ไม่ทำนายผล")
 else :
