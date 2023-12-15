d09eaf01c99b1bd98604943df0a965185d5c1abd
Wit6438 <vbvbvb585@hotmail.com>
2023-12-15 14:27:45 +0700

    .commit

--- Home.py
+++ Home.py
@@ -5,3 +5,11 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('นายวิชญ์ เต็มบริบูรณ์')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/rung1.jfif')
+with col2:
+    st.image('./pic/iris2.jpg')
