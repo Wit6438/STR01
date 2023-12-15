d78f5d65713e0da5255891f228db7766aef9c503
Wit6438 <vbvbvb585@hotmail.com>
2023-12-15 15:50:23 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 
-    st.balloons()
 
 st.header("Wit Temboriboon")
+st.balloons()
 st.title('การทดสอบเขียนเว็บด้วย Python')
 st.header('นายวิชญ์ เต็มบริบูรณ์')
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
